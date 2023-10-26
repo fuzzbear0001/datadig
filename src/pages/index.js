eb87cb3fee28a91de74b2fb5cd15ff348466e176
fuzzbear (bot dev) <58638135+fuzzbear0001@users.noreply.github.com>
2023-10-26 17:48:38 +0100

    github repo update

--- src/pages/index.js
+++ src/pages/index.js
@@ -192,7 +192,7 @@ export default function Home() {
           </a>
         </div>
         <a
-          href="https://github.com/your-github-repo"
+          href="https://github.com/fuzzbear0001/datadig"
           target="_blank"
           className="mt-4 lg:mt-0 text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30"
         >
